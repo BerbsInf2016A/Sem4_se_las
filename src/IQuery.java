3ba30ad08743eb995eb5bd06d2cd565c33da4c5f
André Berberich <Andre.Berberich@tecalliance.net>
2018-02-01 11:15:38 +0100

    Fixed query 6

--- src/IQuery.java
+++ src/IQuery.java
@@ -1,10 +1,12 @@
+import java.util.List;
+
 public interface IQuery {
     void executeSQL01();
     void executeSQL02();
     void executeSQL03();
     void executeSQL04();
     void executeSQL05();
-    void executeSQL06();
+    List<Integer> executeSQL06(List<Record> records);
     void executeSQL07();
     void executeSQL08();
     void executeSQL09();
--- src/Query.java
+++ src/Query.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Query implements  IQuery {
     @Override
     public void executeSQL01() {
@@ -24,9 +27,26 @@ public class Query implements  IQuery {
 
     }
 
-    @Override
-    public void executeSQL06() {
+    public List<Integer> executeSQL06(List<Record> records) {
+        List<String> sources = Arrays.asList("a", "d");
+        List<String> destinations = Arrays.asList("f", "e");
+        Comparator<Record> descendingWeightComparator = (Record record1 , Record record2) -> (int)(record2.getWeight() - record1.getWeight());
+        Comparator<Record> ascendingDestinationComparator = (Record record1 ,Record record2) -> (int)(record1.getDestination().compareTo(record2.getDestination()));
 
+        List<Record> data = records.stream()
+                .filter(record -> sources.contains(record.getSource().toLowerCase()))
+                .filter(record -> destinations.contains(record.getDestination().toLowerCase()))
+                .filter(record -> record.getType().toLowerCase().equals("b"))
+                .filter(record -> record.getWeight() >= 29)
+                .filter(record -> record.getCustoms().toLowerCase().equals("y"))
+                .filter(record -> record.getExtendedSecurityCheck().toLowerCase().equals("y"))
+                .sorted(ascendingDestinationComparator)
+                .collect(Collectors.toList())
+                .stream()
+                .sorted(descendingWeightComparator)
+                .collect(Collectors.toList());
+        List<Integer> result = data.stream().map(record -> record.getId()).collect(Collectors.toList());
+        return result;
     }
 
     @Override
