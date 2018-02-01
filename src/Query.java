d95ce95cc7ff730c4e4b9e38cc2d7b7c70d3386b
André Berberich <Andre.Berberich@tecalliance.net>
2018-02-01 09:51:56 +0100

    Added query class for las implementation

---
+++ src/Query.java
@@ -0,0 +1,61 @@
+public class Query implements  IQuery {
+    @Override
+    public void executeSQL01() {
+
+    }
+
+    @Override
+    public void executeSQL02() {
+
+    }
+
+    @Override
+    public void executeSQL03() {
+
+    }
+
+    @Override
+    public void executeSQL04() {
+
+    }
+
+    @Override
+    public void executeSQL05() {
+
+    }
+
+    @Override
+    public void executeSQL06() {
+
+    }
+
+    @Override
+    public void executeSQL07() {
+
+    }
+
+    @Override
+    public void executeSQL08() {
+
+    }
+
+    @Override
+    public void executeSQL09() {
+
+    }
+
+    @Override
+    public void executeSQL10() {
+
+    }
+
+    @Override
+    public void executeSQL11() {
+
+    }
+
+    @Override
+    public void executeSQL12() {
+
+    }
+}
